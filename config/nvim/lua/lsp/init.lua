ce75fe50db7c6628cd2d1b20f9f03cfbc1d48c03
Khalid J <khalid.y96@outlook.com>
2022-02-01 08:29:47 +0300

    Configure diagnostics display

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -4,3 +4,41 @@ require("lsp.gdscript")
 require("lsp.lua")
 require("lsp.python")
 require("lsp.rust")
+
+-- Diagnostics configuration
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+vim.diagnostic.config({
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+  signs = {
+    active = signs,
+  },
+  severity_sort = true,
+  underline = true,
+  update_in_insert = true,
+  virtual_text = false,
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "rounded",
+})
